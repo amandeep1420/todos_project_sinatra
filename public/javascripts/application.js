b3c5e6ccb06c037ef664dcd8cfcb59d3f88dd007
amandeep1420 <amandeep1420@gmail.com>
2021-09-13 19:19:16 +0000

    Deletion confirmation popup added.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,6 +4,11 @@ $(function() {
     event.preventDefault();
     event.stopPropagation();
     
+    var ok = confirm("This cannot be undone - click OK to confirm deletion.");
+    if (ok) { 
+      this.submit();
+    }
+    
   });
   
 });
